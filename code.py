89025a98ab09394db01ae4c5c2b3b3d4fd39ff7d
pranay kumar <pranaykumargalipelly99@gmail.com>
2024-05-29 13:53:39 -0500

    commiting code and example

---
+++ code.py
@@ -0,0 +1,10 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print hello
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> print "hello"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> a=1
+>>> b=2
+>>> a+b
+3
